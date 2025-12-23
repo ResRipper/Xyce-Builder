d10db7f82ea294f5853704eb99acdc433cba2c85
ResRipper <resripper@connective.link>
2025-12-23 16:48:56 +0800

    Update trilinos.sh
    - Change to new CMake build system

--- trilinos.sh
+++ trilinos.sh
@@ -4,14 +4,14 @@
 
 # Build Trilinos
 # Options:
-#       - TRILINOS_VER (str): Trilinos version (e.g. 12-12-1), recommended by the official build guide 
+#       - TRILINOS_VER (str): Trilinos version (e.g. 14-4-0), recommended by the official build guide
 #       - PARALLEL (boolen): Compiled for parallel
 
-TRILINOS_VER=${TRILINOS_VER:-12-12-1}
 PARALLEL=${PARALLEL:-true}
 
 SRC_DIR=$HOME/Trilinos
-BUILD_PATH=$HOME/Trilinos_bin
+BIN_PATH=$HOME/Trilinos_bin
+XYCE_SRC=$HOME/Xyce
 
 LIB_PATH=/usr/lib64
 SUITESPARSE_INCLUDE=/usr/include/suitesparse
@@ -20,68 +20,37 @@ FLAGS="-O3 -fPIC"
 
 # Build config
 BUILD_CONFIG=(
-    '-DCMAKE_MAKE_PROGRAM=make'
-    '-DTrilinos_ENABLE_NOX=ON'
-        '-DNOX_ENABLE_LOCA=ON'
-    '-DTrilinos_ENABLE_EpetraExt=ON'
-        '-DEpetraExt_BUILD_BTF=ON'
-        '-DEpetraExt_BUILD_EXPERIMENTAL=ON'
-        '-DEpetraExt_BUILD_GRAPH_REORDERINGS=ON'
-    '-DTrilinos_ENABLE_TrilinosCouplings=ON'
-    '-DTrilinos_ENABLE_Ifpack=ON'
-    '-DTrilinos_ENABLE_AztecOO=ON'
-    '-DTrilinos_ENABLE_Belos=ON'
-    '-DTrilinos_ENABLE_Teuchos=ON'
-    '-DTrilinos_ENABLE_COMPLEX_DOUBLE=ON'
-    '-DTrilinos_ENABLE_Amesos=ON'
-        '-DAmesos_ENABLE_KLU=ON'
-    '-DTrilinos_ENABLE_Amesos2=ON'
-        '-DAmesos2_ENABLE_KLU2=ON'
-        '-DAmesos2_ENABLE_Basker=ON'
-    '-DTrilinos_ENABLE_Sacado=ON'
-    '-DTrilinos_ENABLE_Stokhos=ON'
-    '-DTrilinos_ENABLE_Kokkos=ON'
-    '-DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF'
-    '-DTrilinos_ENABLE_CXX11=ON'
-    '-DTPL_ENABLE_AMD=ON'
-    '-DTPL_ENABLE_BLAS=ON'
-    '-DTPL_ENABLE_LAPACK=ON'
+    "-D AMD_LIBRARY_DIRS=${LIB_PATH}"
+    "-D TPL_AMD_INCLUDE_DIRS=${SUITESPARSE_INCLUDE}"
 )
 
-# Specify compiler
+# Specify compilers and cmake file
 if [ "$PARALLEL" = true ]; then
     BUILD_CONFIG+=(
         '-DCMAKE_C_COMPILER=mpicc'
         '-DCMAKE_CXX_COMPILER=mpic++'
         '-DCMAKE_Fortran_COMPILER=mpif77'
+        "-C ${XYCE_SRC}/cmake/trilinos/trilinos-MPI-base.cmake"
     )
+
 else
     BUILD_CONFIG+=(
         '-DCMAKE_C_COMPILER=gcc'
         '-DCMAKE_CXX_COMPILER=g++'
         '-DCMAKE_Fortran_COMPILER=gfortran'
+        "-C ${XYCE_SRC}/cmake/trilinos/trilinos-base.cmake"
     )
 fi
 
-mkdir -p "$BUILD_PATH"
+mkdir -p "$BIN_PATH"
+mkdir -p "$SRC_DIR"/build && cd "$SRC_DIR"/build || exit
 
-# Fetch source code and build
-git clone \
-    --depth 1 \
-    --branch  trilinos-release-"${TRILINOS_VER}" \
-    https://github.com/trilinos/Trilinos \
-    "$SRC_DIR"
-
-cd "$BUILD_PATH" || exit
-
+# Build
 cmake \
--G 'Unix Makefiles' \
--DCMAKE_INSTALL_PREFIX="${BUILD_PATH}" \
--DAMD_LIBRARY_DIRS="${LIB_PATH}" \
--DTPL_AMD_INCLUDE_DIRS="${SUITESPARSE_INCLUDE}" \
--DCMAKE_CXX_FLAGS="$FLAGS" \
--DCMAKE_C_FLAGS="$FLAGS" \
--DCMAKE_Fortran_FLAGS="$FLAGS" \
-${BUILD_CONFIG[*]} "$SRC_DIR"
+    -D CMAKE_INSTALL_PREFIX="${BIN_PATH}" \
+    -D CMAKE_C_FLAGS="$FLAGS" \
+    -D CMAKE_CXX_FLAGS="$FLAGS" \
+    -D CMAKE_Fortran_FLAGS="$FLAGS" \
+    ${BUILD_CONFIG[*]} "$SRC_DIR"
 
-make -j "$(nproc)"
+cmake --build . -j "$(nproc)" -t install
