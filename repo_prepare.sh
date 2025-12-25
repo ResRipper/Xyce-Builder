5570e9c8acdd064d5d1010df5549e787e82d5a15
ResRipper <resripper@connective.link>
2025-12-25 16:23:53 +0800

    Add test suite

--- repo_prepare.sh
+++ repo_prepare.sh
@@ -9,15 +9,25 @@ TRILINOS_SRC=$HOME/Trilinos
 
 XYCE_VER=${XYCE_VER:-7.10.0}
 XYCE_SRC=$HOME/Xyce
+TEST_SUITE_PATH=$HOME/Xyce_Regression
 
+# Trilinos
 git clone \
     --depth 1 \
     --branch  trilinos-release-"${TRILINOS_VER}" \
     https://github.com/trilinos/Trilinos \
     "$TRILINOS_SRC"
 
+# Xyce
 git clone \
     --depth 1 \
     --branch  Release-"${XYCE_VER}" \
     https://github.com/Xyce/Xyce \
     "$XYCE_SRC"
+
+# Test suite
+git clone \
+    --depth 1 \
+    --branch  Release-"${XYCE_VER}" \
+    https://github.com/Xyce/Xyce_Regression \
+    "$TEST_SUITE_PATH"
